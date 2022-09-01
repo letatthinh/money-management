10d62835f54a863b6d6b41ccc6e0dc62af259736
Lê Tất Thịnh <ltti@netcompany.com>
2022-09-01 16:06:53 +0700

    Init project with database creation and connection

---
+++ MoneyManagement/4 - Data/DatabaseContexts/MoneyManagementDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Common.Constants;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.DatabaseContexts
+{
+    public class MoneyManagementDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DatabaseConstants.ConnectionString);
+        }
+    }
+}
---
+++ MoneyManagement/Common/Constants/DatabaseConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Constants
+{
+    public class DatabaseConstants
+    {
+        public const string ConnectionString = "Data Source=DESKTOP-EEUTCFV;Initial Catalog=moneymanagement;Integrated Security=True";
+    }
+}
