9e8ddedda437ddae486aa57361f662d8ce6f7a21
Lê Tất Thịnh <ltti@netcompany.com>
2022-09-18 16:20:58 +0700

    update query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,13 +1,44 @@
 DROP TABLE [dbo].[__EFMigrationsHistory];
-DROP TABLE [transaction].[Income];
 
+DROP TABLE [contact].[CustomContactIncomeDetail];
+DROP TABLE [contact].[CustomContactExpenseDetail];
+DROP TABLE [contact].[CustomContactLoanOwner];
+DROP TABLE [contact].[CustomContactLoanPaymentDetail];
+DROP TABLE [contact].[CustomContactDebtor];
+DROP TABLE [contact].[CustomContactLendingPaymentDetail];
+DROP TABLE [contact].[CustomContactInvestment];
 DROP TABLE [contact].[CustomContactEmail];
 DROP TABLE [contact].[Person];
 DROP TABLE [contact].[Place];
 DROP TABLE [contact].[CustomContact];
 
+DROP TABLE [user].[UserContactIncomeDetail];
+DROP TABLE [user].[UserContactExpenseDetail];
+DROP TABLE [user].[UserContactLoanOwner];
+DROP TABLE [user].[UserContactLoanPaymentDetail];
+DROP TABLE [user].[UserContactDebtor];
+DROP TABLE [user].[UserContactLendingPaymentDetail];
+DROP TABLE [user].[UserContactInvestment];
+
+DROP TABLE [moneytransaction].[IncomeDetail];
+DROP TABLE [moneytransaction].[ExpenseDetail];
+DROP TABLE [moneytransaction].[LoanPaymentDetail];
+DROP TABLE [moneytransaction].[LendingPaymentDetail];
+DROP TABLE [moneytransaction].[Income];
+DROP TABLE [moneytransaction].[Expense];
+DROP TABLE [moneytransaction].[Loan];
+DROP TABLE [moneytransaction].[Lending];
+DROP TABLE [moneytransaction].[Investment];
+DROP TABLE [moneytransaction].[MoneyTransaction];
+
+DROP TABLE [user].[UserInvestmentCategory];
+DROP TABLE [user].[UserMoneyTransactionCategory];
 DROP TABLE [user].[UserEmail];
 DROP TABLE [user].[UserContacts];
 DROP TABLE [user].[UserRoles];
 DROP TABLE [user].[Role];
 DROP TABLE [user].[User];
+
+
+
+DROP TABLE ;
